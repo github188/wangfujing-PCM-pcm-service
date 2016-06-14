d39c208b8b275a47bfc54a68b58fa695e81453c4
wangchuan <379101605@qq.com>
2016-06-14 10:39:17 +0800

    拍照展示分类属性及属性值

---
+++ src/main/java/com/wangfj/product/category/domain/vo/PcmPropValueResultDto.java
@@ -0,0 +1,22 @@
+/**
+ * @Probject Name: pcm-service
+ * @Path: com.wangfj.product.category.domain.voPcmPropValueResultDto.java
+ * @Create By wangc
+ * @Create In 2016年6月8日 下午2:57:26
+ * TODO
+ */
+package com.wangfj.product.category.domain.vo;
+
+/**
+ * @Class Name PcmPropValueResultDto
+ * @Author wangc
+ * @Create In 2016年6月8日
+ */
+public class PcmPropValueResultDto {
+
+	private Long sid;
+	private String code;
+	private String name;
+	
+	
+}
