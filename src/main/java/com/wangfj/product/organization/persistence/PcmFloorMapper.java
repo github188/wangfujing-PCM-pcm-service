b2a80738cdd3858efc706c3b94674d830a37352e
wangxuan <1160880871@qq.com>
2016-08-29 17:05:42 +0800

    供应商平台根据门店编码查询楼层

--- src/main/java/com/wangfj/product/organization/persistence/PcmFloorMapper.java
+++ src/main/java/com/wangfj/product/organization/persistence/PcmFloorMapper.java
@@ -11,162 +11,169 @@ import com.wangfj.product.organization.domain.vo.PcmFloorResultDto;
 import com.wangfj.product.organization.domain.vo.SelectPcmFloorDto;
 
 /**
- * 
  * @Class Name PcmFloorMapper
  * @Author yedong
  * @Create In 2015年7月3日
  */
 public interface PcmFloorMapper extends BaseMapper<PcmFloor> {
-	/**
-	 * 删除
-	 * 
-	 * @Methods Name deleteByPrimaryKey
-	 * @Create In 2015年7月3日 By yedong
-	 * @param sid
-	 * @return int
-	 */
-	int deleteByPrimaryKey(Long sid);
+    /**
+     * 删除
+     *
+     * @param sid
+     * @return int
+     * @Methods Name deleteByPrimaryKey
+     * @Create In 2015年7月3日 By yedong
+     */
+    int deleteByPrimaryKey(Long sid);
 
-	/**
-	 * 添加
-	 * 
-	 * @Methods Name insertSelective
-	 * @Create In 2015年7月3日 By yedong
-	 * @param record
-	 * @return int
-	 */
-	int insertSelective(PcmFloor record);
+    /**
+     * 添加
+     *
+     * @param record
+     * @return int
+     * @Methods Name insertSelective
+     * @Create In 2015年7月3日 By yedong
+     */
+    int insertSelective(PcmFloor record);
 
-	/**
-	 * 修改
-	 * 
-	 * @Methods Name updateByPrimaryKeySelective
-	 * @Create In 2015年7月3日 By yedong
-	 * @param record
-	 * @return int
-	 */
-	int updateByPrimaryKeySelective(PcmFloor record);
+    /**
+     * 修改
+     *
+     * @param record
+     * @return int
+     * @Methods Name updateByPrimaryKeySelective
+     * @Create In 2015年7月3日 By yedong
+     */
+    int updateByPrimaryKeySelective(PcmFloor record);
 
-	/**
-	 * 楼层信息发布
-	 * 
-	 * @Methods Name findFloorByParamFromPcm
-	 * @Create In 2015年7月16日 By yedong
-	 * @param paramMap
-	 * @return List<PcmFloorDto>
-	 */
-	List<Map<String, Object>> findFloorByParamFromPcm(Map<String, Object> paramMap);
+    /**
+     * 楼层信息发布
+     *
+     * @param paramMap
+     * @return List<PcmFloorDto>
+     * @Methods Name findFloorByParamFromPcm
+     * @Create In 2015年7月16日 By yedong
+     */
+    List<Map<String, Object>> findFloorByParamFromPcm(Map<String, Object> paramMap);
 
-	/**
-	 * 判重门店编码和楼层名
-	 * 
-	 * @Methods Name selectCountByFloor
-	 * @Create In 2015年7月24日 By wuxiong
-	 * @param paramMap
-	 * @return Integer
-	 */
-	Integer selectCountByFloor(Map<String, Object> paramMap);
+    /**
+     * 判重门店编码和楼层名
+     *
+     * @param paramMap
+     * @return Integer
+     * @Methods Name selectCountByFloor
+     * @Create In 2015年7月24日 By wuxiong
+     */
+    Integer selectCountByFloor(Map<String, Object> paramMap);
 
-	Integer selectCountByFloors(Map<String, Object> paramMap);
+    Integer selectCountByFloors(Map<String, Object> paramMap);
 
-	/**
-	 * 分页查询门店名称，楼层名称，楼层编码
-	 * 
-	 * @Methods Name findFloorByParam
-	 * @Create In 2015年7月27日 By wuxiong
-	 * @param paramMap
-	 * @return List<Map<String,Object>>
-	 */
-	List<HashMap<String, Object>> findFloorByParam(Map<String, Object> paramMap);
+    /**
+     * 分页查询门店名称，楼层名称，楼层编码
+     *
+     * @param paramMap
+     * @return List<Map<String,Object>>
+     * @Methods Name findFloorByParam
+     * @Create In 2015年7月27日 By wuxiong
+     */
+    List<HashMap<String, Object>> findFloorByParam(Map<String, Object> paramMap);
 
-	Integer getFloorCount(Map<String, Object> paramMap);
+    Integer getFloorCount(Map<String, Object> paramMap);
 
-	PcmFloorDto findFloorBySid(Long sid);
+    PcmFloorDto findFloorBySid(Long sid);
 
-	/**
-	 * 测试数据
-	 * 
-	 * @param dto
-	 * @return
-	 */
-	Integer selectFloorByParam(PcmFloorDto dto);
+    /**
+     * 测试数据
+     *
+     * @param dto
+     * @return
+     */
+    Integer selectFloorByParam(PcmFloorDto dto);
 
-	/**
-	 * 根据ShopSid查询此门店下的楼层列表信息
-	 * 
-	 * @Methods Name getFloorsByShopSid
-	 * @Create In 2015-8-24 By chengsj
-	 * @param shopSid
-	 * @return List<PcmFloor>
-	 */
-	List<Map<String, Object>> getFloorsByShopSid(Long shopSid);
+    /**
+     * 根据ShopSid查询此门店下的楼层列表信息
+     *
+     * @param shopSid
+     * @return List<PcmFloor>
+     * @Methods Name getFloorsByShopSid
+     * @Create In 2015-8-24 By chengsj
+     */
+    List<Map<String, Object>> getFloorsByShopSid(Long shopSid);
 
-	/**
-	 * 根据sid查询
-	 * 
-	 * @Methods Name selectByPrimaryKey
-	 * @Create In 2015-8-24 By wangxuan
-	 * @param sid
-	 * @return PcmFloor
-	 */
-	PcmFloor selectByPrimaryKey(Long sid);
+    /**
+     * 根据sid查询
+     *
+     * @param sid
+     * @return PcmFloor
+     * @Methods Name selectByPrimaryKey
+     * @Create In 2015-8-24 By wangxuan
+     */
+    PcmFloor selectByPrimaryKey(Long sid);
 
-	/**
-	 * 查询楼层List
-	 * 
-	 * @param dto
-	 * @return
-	 */
-	List<PcmFloorResultDto> selectFloorListByParam(SelectPcmFloorDto dto);
+    /**
+     * 查询楼层List
+     *
+     * @param dto
+     * @return
+     */
+    List<PcmFloorResultDto> selectFloorListByParam(SelectPcmFloorDto dto);
 
-	/**
-	 * 分页查询
-	 * 
-	 * @Methods Name selectPageByPara
-	 * @Create In 2015-8-25 By niuzhifan
-	 * @param dto
-	 * @return List<PcmFloorResultDto>
-	 */
-	List<PcmFloorResultDto> selectPageByPara(SelectPcmFloorDto dto);
+    /**
+     * 供应商平台查询楼层
+     *
+     * @param paramMap
+     * @return
+     */
+    List<PcmFloorResultDto> findFloorByParaToSupplier(Map<String, Object> paramMap);
 
-	/**
-	 * 查询总数量
-	 * 
-	 * @Methods Name getPageCountByPara
-	 * @Create In 2015-8-25 By niuzhifan
-	 * @param dto
-	 * @return Integer
-	 */
-	Integer getPageCountByPara(SelectPcmFloorDto dto);
+    /**
+     * 分页查询
+     *
+     * @param dto
+     * @return List<PcmFloorResultDto>
+     * @Methods Name selectPageByPara
+     * @Create In 2015-8-25 By niuzhifan
+     */
+    List<PcmFloorResultDto> selectPageByPara(SelectPcmFloorDto dto);
 
-	/**
-	 * 根据门店编码查询楼层的信息，并按指定格式输出 (移动工作台调用主数据获取楼层信息列表)
-	 * 
-	 * @param map
-	 * @return
-	 */
-	List<Map<String, Object>> getFloorsByShopCode(Map<String, Object> map);
+    /**
+     * 查询总数量
+     *
+     * @param dto
+     * @return Integer
+     * @Methods Name getPageCountByPara
+     * @Create In 2015-8-25 By niuzhifan
+     */
+    Integer getPageCountByPara(SelectPcmFloorDto dto);
 
-	/**
-	 * 根据门店编码和楼层编码（可为空）查询专柜的信息，并按指定格式输出 (移动工作台调用主数据获取门店对应楼层下所有专柜信)
-	 * 
-	 * @param map
-	 * @return
-	 */
-	List<Map<String, Object>> getFloorsByShopAndFloorCode(Map<String, Object> map);
+    /**
+     * 根据门店编码查询楼层的信息，并按指定格式输出 (移动工作台调用主数据获取楼层信息列表)
+     *
+     * @param map
+     * @return
+     */
+    List<Map<String, Object>> getFloorsByShopCode(Map<String, Object> map);
 
-	/**
-	 * 楼层信息发布
-	 * 
-	 * @Methods Name getFloorsAndOrg
-	 * @Create In 2015/9/8 By nzf
-	 * @param paramMap
-	 * @return List<PcmFloorDto>
-	 */
-	List<Map<String, Object>> getFloorsAndOrg(Map<String, Object> map);
+    /**
+     * 根据门店编码和楼层编码（可为空）查询专柜的信息，并按指定格式输出 (移动工作台调用主数据获取门店对应楼层下所有专柜信)
+     *
+     * @param map
+     * @return
+     */
+    List<Map<String, Object>> getFloorsByShopAndFloorCode(Map<String, Object> map);
 
-	List<Map<String, Object>> pushFloorData(Map<String, Object> paramMap);
+    /**
+     * 楼层信息发布
+     *
+     * @param paramMap
+     * @return List<PcmFloorDto>
+     * @Methods Name getFloorsAndOrg
+     * @Create In 2015/9/8 By nzf
+     */
+    List<Map<String, Object>> getFloorsAndOrg(Map<String, Object> map);
 
-	List<PcmFloor> selectListByParam(Map<String, Object> paramMap);
+    List<Map<String, Object>> pushFloorData(Map<String, Object> paramMap);
+
+    List<PcmFloor> selectListByParam(Map<String, Object> paramMap);
 
 }
--- src/main/java/com/wangfj/product/organization/service/intf/IPcmFloorService.java
+++ src/main/java/com/wangfj/product/organization/service/intf/IPcmFloorService.java
@@ -12,82 +12,90 @@ import com.wangfj.product.organization.domain.vo.SelectPcmFloorDto;
 
 public interface IPcmFloorService {
 
-	public List<PcmFloorDto> findFloorByParamFromPcm(Map<String, Object> paramMap)
-			throws IllegalAccessException, InvocationTargetException;
+    public List<PcmFloorDto> findFloorByParamFromPcm(Map<String, Object> paramMap)
+            throws IllegalAccessException, InvocationTargetException;
 
-	Integer getCountByParam(Map<String, Object> map);
+    Integer getCountByParam(Map<String, Object> map);
 
-	Page<HashMap<String, Object>> findFloorFromPCM(PcmFloorDto floordto, Page<PcmFloorDto> pagedto);
+    Page<HashMap<String, Object>> findFloorFromPCM(PcmFloorDto floordto, Page<PcmFloorDto> pagedto);
 
-	PcmFloorDto selectByFloorSid(Long sid);
+    PcmFloorDto selectByFloorSid(Long sid);
 
-	Integer deleteFloor(PcmFloorDto dto);
+    Integer deleteFloor(PcmFloorDto dto);
 
-	/**
-	 * 查询此门店下所有楼层列表
-	 * 
-	 * @Methods Name getFloorsByShopSid
-	 * @Create In 2015-8-24 By chengsj
-	 * @param shopSid
-	 * @return List<Map<String,Object>>
-	 */
-	List<Map<String, Object>> getFloorsByShopSid(Long shopSid);
+    /**
+     * 查询此门店下所有楼层列表
+     *
+     * @param shopSid
+     * @return List<Map<String,Object>>
+     * @Methods Name getFloorsByShopSid
+     * @Create In 2015-8-24 By chengsj
+     */
+    List<Map<String, Object>> getFloorsByShopSid(Long shopSid);
 
-	/**
-	 * 分页查询
-	 * 
-	 * @Methods Name findPageFloor
-	 * @Create In 2015-8-25 By niuzhifan
-	 * @param selectFloorDto
-	 * @return Page<PcmFloorResultDto>
-	 */
-	Page<PcmFloorResultDto> findPageFloor(SelectPcmFloorDto selectFloorDto);
+    /**
+     * 分页查询
+     *
+     * @param selectFloorDto
+     * @return Page<PcmFloorResultDto>
+     * @Methods Name findPageFloor
+     * @Create In 2015-8-25 By niuzhifan
+     */
+    Page<PcmFloorResultDto> findPageFloor(SelectPcmFloorDto selectFloorDto);
 
-	/**
-	 * 查询楼层List
-	 * 
-	 * @param dto
-	 * @return
-	 */
-	List<PcmFloorResultDto> selectFloorListByParam(SelectPcmFloorDto dto);
+    /**
+     * 查询楼层List
+     *
+     * @param dto
+     * @return
+     */
+    List<PcmFloorResultDto> selectFloorListByParam(SelectPcmFloorDto dto);
 
-	/**
-	 * 移动工作台调用主数据获取楼层信息列表
-	 * 
-	 * @create in 2015-8-31 nzf
-	 * @param dto
-	 * @return
-	 */
-	List<Map<String, Object>> selectFloorByShopCode(PcmFloorDto dto);
+    /**
+     * 供应商平台根据门店编码查询楼层
+     *
+     * @param paramMap
+     * @return
+     */
+    List<PcmFloorResultDto> findFloorByParaToSupplier(Map<String, Object> paramMap);
 
-	/**
-	 * 移动工作台调用主数据获取门店对应楼层下所有专柜信
-	 * 
-	 * @create in 2015-8-31 nzf
-	 * @param dto
-	 * @return
-	 */
-	List<Map<String, Object>> selectFloorsByShopAndFloorCode(PcmFloorDto dto);
+    /**
+     * 移动工作台调用主数据获取楼层信息列表
+     *
+     * @param dto
+     * @return
+     * @create in 2015-8-31 nzf
+     */
+    List<Map<String, Object>> selectFloorByShopCode(PcmFloorDto dto);
 
-	/**
-	 * 主数据ERP发送楼层字典到其他系统(增量下发)
-	 */
-	List<Map<String, Object>> selectFloorsANDOrg(Map<String, Object> dtoMap);
+    /**
+     * 移动工作台调用主数据获取门店对应楼层下所有专柜信
+     *
+     * @param dto
+     * @return
+     * @create in 2015-8-31 nzf
+     */
+    List<Map<String, Object>> selectFloorsByShopAndFloorCode(PcmFloorDto dto);
 
-	List<Map<String, Object>> pushFloorData(Map<String, Object> para);
+    /**
+     * 主数据ERP发送楼层字典到其他系统(增量下发)
+     */
+    List<Map<String, Object>> selectFloorsANDOrg(Map<String, Object> dtoMap);
 
-	/**
-	 * 楼层判重
-	 * 
-	 * @Methods Name isExistence
-	 * @Create In 2015-11-26 By wangxuan
-	 * @param para
-	 * @return boolean
-	 */
-	boolean isExistence(Map<String, Object> para);
+    List<Map<String, Object>> pushFloorData(Map<String, Object> para);
 
-	Map<String, Object> addFloor(PcmFloorDto dto);
+    /**
+     * 楼层判重
+     *
+     * @param para
+     * @return boolean
+     * @Methods Name isExistence
+     * @Create In 2015-11-26 By wangxuan
+     */
+    boolean isExistence(Map<String, Object> para);
 
-	Map<String, Object> modifyFloor(PcmFloorDto dto);
+    Map<String, Object> addFloor(PcmFloorDto dto);
+
+    Map<String, Object> modifyFloor(PcmFloorDto dto);
 
 }
