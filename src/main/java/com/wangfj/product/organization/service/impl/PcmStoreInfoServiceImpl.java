d53ed9513b9905ed3b536235ee10319f7dcc6e9e
yedong <529890739@qq.com>
2016-11-15 14:11:05 +0800

    是否下发

--- src/main/java/com/wangfj/product/organization/service/impl/PcmStoreInfoServiceImpl.java
+++ src/main/java/com/wangfj/product/organization/service/impl/PcmStoreInfoServiceImpl.java
@@ -1,5 +1,13 @@
 package com.wangfj.product.organization.service.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.wangfj.core.framework.base.page.Page;
 import com.wangfj.product.organization.domain.entity.PcmStoreInfo;
 import com.wangfj.product.organization.domain.vo.PcmStoreInfoQueryDto;
@@ -8,112 +16,109 @@ import com.wangfj.product.organization.domain.vo.PcmStoreInfoToSupplierDto;
 import com.wangfj.product.organization.persistence.PcmStoreInfoMapper;
 import com.wangfj.product.organization.service.intf.IPcmStoreInfoService;
 import com.wangfj.util.Constants;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 /**
- * 门店信息
- * Created by wangxuan on 2016-08-25 0025.
+ * 门店信息 Created by wangxuan on 2016-08-25 0025.
  */
 @Service
 public class PcmStoreInfoServiceImpl implements IPcmStoreInfoService {
 
-    @Autowired
-    private PcmStoreInfoMapper storeInfoMapper;
+	@Autowired
+	private PcmStoreInfoMapper storeInfoMapper;
 
-    private static final Logger logger = LoggerFactory.getLogger(PcmStoreInfoServiceImpl.class);
+	private static final Logger logger = LoggerFactory.getLogger(PcmStoreInfoServiceImpl.class);
 
-    /**
-     * 门店信息分页查询
-     *
-     * @param queryDto
-     * @return
-     */
-    @Override
-    public Page<PcmStoreInfoResultDto> findPageStoreInfo(PcmStoreInfoQueryDto queryDto) {
-        logger.debug("start findPageStoreInfo(),param:" + queryDto.toString());
-        Page<PcmStoreInfoResultDto> page = new Page<PcmStoreInfoResultDto>();
+	public List<PcmStoreInfo> selectListByParam(PcmStoreInfo storeInfo) {
+		return storeInfoMapper.selectListByParam(storeInfo);
+	}
 
-        Integer currentPage = queryDto.getCurrentPage();
-        Integer pageSize = queryDto.getPageSize();
-        if (currentPage != null) {
-            page.setCurrentPage(currentPage);
-        } else {
-            page.setCurrentPage(Constants.PUBLIC_1);
-        }
+	/**
+	 * 门店信息分页查询
+	 *
+	 * @param queryDto
+	 * @return
+	 */
+	@Override
+	public Page<PcmStoreInfoResultDto> findPageStoreInfo(PcmStoreInfoQueryDto queryDto) {
+		logger.debug("start findPageStoreInfo(),param:" + queryDto.toString());
+		Page<PcmStoreInfoResultDto> page = new Page<PcmStoreInfoResultDto>();
 
-        if (pageSize != null) {
-            page.setPageSize(pageSize);
-        } else {
-            page.setPageSize(Constants.PUBLIC_10);
-        }
+		Integer currentPage = queryDto.getCurrentPage();
+		Integer pageSize = queryDto.getPageSize();
+		if (currentPage != null) {
+			page.setCurrentPage(currentPage);
+		} else {
+			page.setCurrentPage(Constants.PUBLIC_1);
+		}
 
-        // 查询总数量
-        Integer count = storeInfoMapper.getPageCountByPara(queryDto);
-        page.setCount(count);
+		if (pageSize != null) {
+			page.setPageSize(pageSize);
+		} else {
+			page.setPageSize(Constants.PUBLIC_10);
+		}
 
-        // 分页查询
-        queryDto.setStart(page.getStart());
-        queryDto.setLimit(page.getLimit());
-        List<PcmStoreInfoResultDto> storeInfoResultDtoList = storeInfoMapper.selectPageByPara(queryDto);
+		// 查询总数量
+		Integer count = storeInfoMapper.getPageCountByPara(queryDto);
+		page.setCount(count);
 
-        page.setList(storeInfoResultDtoList);
+		// 分页查询
+		queryDto.setStart(page.getStart());
+		queryDto.setLimit(page.getLimit());
+		List<PcmStoreInfoResultDto> storeInfoResultDtoList = storeInfoMapper
+				.selectPageByPara(queryDto);
 
-        logger.info("end findPageStoreInfo(),return:" + storeInfoResultDtoList.toString());
-        return page;
-    }
+		page.setList(storeInfoResultDtoList);
 
-    /**
-     * 查询门店信息List
-     *
-     * @param queryDto
-     * @return
-     */
-    @Override
-    public List<PcmStoreInfoResultDto> findListStoreInfo(PcmStoreInfoQueryDto queryDto) {
-        logger.debug("start findListStoreInfo(),param:" + queryDto.toString());
-        List<PcmStoreInfoResultDto> storeInfoResultDtoList = storeInfoMapper.selectPageByPara(queryDto);
-        logger.info("end findListStoreInfo(),return:" + storeInfoResultDtoList.toString());
-        return storeInfoResultDtoList;
-    }
+		logger.info("end findPageStoreInfo(),return:" + storeInfoResultDtoList.toString());
+		return page;
+	}
 
-    /**
-     * 供应商平台根据门店编码查询门店信息
-     *
-     * @param queryDto
-     * @return
-     */
-    @Override
-    public List<PcmStoreInfoToSupplierDto> findStoreInfoByParaToSupplier(PcmStoreInfoQueryDto queryDto) {
-        logger.info("start findStoreInfoByParaToSupplier(),param:" + queryDto.toString());
-        List<PcmStoreInfoToSupplierDto> resultList = new ArrayList<PcmStoreInfoToSupplierDto>();
+	/**
+	 * 查询门店信息List
+	 *
+	 * @param queryDto
+	 * @return
+	 */
+	@Override
+	public List<PcmStoreInfoResultDto> findListStoreInfo(PcmStoreInfoQueryDto queryDto) {
+		logger.debug("start findListStoreInfo(),param:" + queryDto.toString());
+		List<PcmStoreInfoResultDto> storeInfoResultDtoList = storeInfoMapper
+				.selectPageByPara(queryDto);
+		logger.info("end findListStoreInfo(),return:" + storeInfoResultDtoList.toString());
+		return storeInfoResultDtoList;
+	}
 
-        List<PcmStoreInfoResultDto> resultDtoList = storeInfoMapper.findListByPara(queryDto);
-        if (resultDtoList != null && resultDtoList.size() > 0) {
-            for (PcmStoreInfoResultDto resultDto : resultDtoList) {
-                PcmStoreInfoToSupplierDto toSupplierDto = new PcmStoreInfoToSupplierDto();
-                toSupplierDto.setAddress(resultDto.getRegisteredAddress());
-                toSupplierDto.setBank(resultDto.getBank());
-                toSupplierDto.setBankAccount(resultDto.getBankAccount());
-                toSupplierDto.setFax(resultDto.getFaxNumber());
-                toSupplierDto.setLegal(resultDto.getLegalRepresentative());
-                toSupplierDto.setPostCode(resultDto.getPostCode());
-                toSupplierDto.setProxy(resultDto.getAgent());
-                toSupplierDto.setShopName(resultDto.getOrganizationName());
-                toSupplierDto.setTaxNo(resultDto.getTaxRegistrationNumber());
-                toSupplierDto.setTel(resultDto.getTelephoneNumber());
-                resultList.add(toSupplierDto);
-            }
-        }
-        logger.info("end findStoreInfoByParaToSupplier(),return:" + resultList.toString());
-        return resultList;
-    }
+	/**
+	 * 供应商平台根据门店编码查询门店信息
+	 *
+	 * @param queryDto
+	 * @return
+	 */
+	@Override
+	public List<PcmStoreInfoToSupplierDto> findStoreInfoByParaToSupplier(
+			PcmStoreInfoQueryDto queryDto) {
+		logger.info("start findStoreInfoByParaToSupplier(),param:" + queryDto.toString());
+		List<PcmStoreInfoToSupplierDto> resultList = new ArrayList<PcmStoreInfoToSupplierDto>();
 
+		List<PcmStoreInfoResultDto> resultDtoList = storeInfoMapper.findListByPara(queryDto);
+		if (resultDtoList != null && resultDtoList.size() > 0) {
+			for (PcmStoreInfoResultDto resultDto : resultDtoList) {
+				PcmStoreInfoToSupplierDto toSupplierDto = new PcmStoreInfoToSupplierDto();
+				toSupplierDto.setAddress(resultDto.getRegisteredAddress());
+				toSupplierDto.setBank(resultDto.getBank());
+				toSupplierDto.setBankAccount(resultDto.getBankAccount());
+				toSupplierDto.setFax(resultDto.getFaxNumber());
+				toSupplierDto.setLegal(resultDto.getLegalRepresentative());
+				toSupplierDto.setPostCode(resultDto.getPostCode());
+				toSupplierDto.setProxy(resultDto.getAgent());
+				toSupplierDto.setShopName(resultDto.getOrganizationName());
+				toSupplierDto.setTaxNo(resultDto.getTaxRegistrationNumber());
+				toSupplierDto.setTel(resultDto.getTelephoneNumber());
+				resultList.add(toSupplierDto);
+			}
+		}
+		logger.info("end findStoreInfoByParaToSupplier(),return:" + resultList.toString());
+		return resultList;
+	}
 
 }
--- src/main/java/com/wangfj/product/organization/service/intf/IPcmStoreInfoService.java
+++ src/main/java/com/wangfj/product/organization/service/intf/IPcmStoreInfoService.java
@@ -1,39 +1,41 @@
 package com.wangfj.product.organization.service.intf;
 
+import java.util.List;
+
 import com.wangfj.core.framework.base.page.Page;
+import com.wangfj.product.organization.domain.entity.PcmStoreInfo;
 import com.wangfj.product.organization.domain.vo.PcmStoreInfoQueryDto;
 import com.wangfj.product.organization.domain.vo.PcmStoreInfoResultDto;
 import com.wangfj.product.organization.domain.vo.PcmStoreInfoToSupplierDto;
 
-import java.util.List;
-
 /**
- * 门店信息
- * Created by wangxuan on 2016-08-25 0025.
+ * 门店信息 Created by wangxuan on 2016-08-25 0025.
  */
 public interface IPcmStoreInfoService {
 
-    /**
-     * 门店信息分页查询
-     *
-     * @param queryDto
-     * @return
-     */
-    Page<PcmStoreInfoResultDto> findPageStoreInfo(PcmStoreInfoQueryDto queryDto);
+	public List<PcmStoreInfo> selectListByParam(PcmStoreInfo storeInfo);
 
-    /**
-     * 查询门店信息List
-     *
-     * @param queryDto
-     * @return
-     */
-    List<PcmStoreInfoResultDto> findListStoreInfo(PcmStoreInfoQueryDto queryDto);
+	/**
+	 * 门店信息分页查询
+	 *
+	 * @param queryDto
+	 * @return
+	 */
+	Page<PcmStoreInfoResultDto> findPageStoreInfo(PcmStoreInfoQueryDto queryDto);
 
-    /**
-     * 供应商平台根据门店编码查询门店信息
-     *
-     * @param queryDto
-     * @return
-     */
-    List<PcmStoreInfoToSupplierDto> findStoreInfoByParaToSupplier(PcmStoreInfoQueryDto queryDto);
+	/**
+	 * 查询门店信息List
+	 *
+	 * @param queryDto
+	 * @return
+	 */
+	List<PcmStoreInfoResultDto> findListStoreInfo(PcmStoreInfoQueryDto queryDto);
+
+	/**
+	 * 供应商平台根据门店编码查询门店信息
+	 *
+	 * @param queryDto
+	 * @return
+	 */
+	List<PcmStoreInfoToSupplierDto> findStoreInfoByParaToSupplier(PcmStoreInfoQueryDto queryDto);
 }
