7d3714f466911eff5d1584849ac6d9b90ed6af38
wangxuan <1160880871@qq.com>
2016-11-16 10:57:34 +0800

    判断是否下发给促销公共方法

--- src/main/java/com/wangfj/product/organization/service/impl/PcmStoreInfoServiceImpl.java
+++ src/main/java/com/wangfj/product/organization/service/impl/PcmStoreInfoServiceImpl.java
@@ -23,117 +23,118 @@ import com.wangfj.util.Constants;
 @Service
 public class PcmStoreInfoServiceImpl implements IPcmStoreInfoService {
 
-	@Autowired
-	private PcmStoreInfoMapper storeInfoMapper;
+    @Autowired
+    private PcmStoreInfoMapper storeInfoMapper;
 
-	private static final Logger logger = LoggerFactory.getLogger(PcmStoreInfoServiceImpl.class);
+    private static final Logger logger = LoggerFactory.getLogger(PcmStoreInfoServiceImpl.class);
 
-	public List<PcmStoreInfo> selectListByParam(PcmStoreInfo storeInfo) {
-		return storeInfoMapper.selectListByParam(storeInfo);
-	}
+    public List<PcmStoreInfo> selectListByParam(PcmStoreInfo storeInfo) {
+        return storeInfoMapper.selectListByParam(storeInfo);
+    }
 
-	public boolean getPublish(String storeCode) {
-		PcmStoreInfo storeInfo = new PcmStoreInfo();
-		storeInfo.setStoreCode(storeCode);
-		List<PcmStoreInfo> storeList = selectListByParam(storeInfo);
-		if (storeList != null && storeList.size() > 0) {
-			if (storeList.get(0).getField5().equals("1")) {
-				return true;
-			} else {
-				return false;
-			}
-		} else {
-			return true;
-		}
-	}
+    public boolean getPublish(String storeCode) {
+        PcmStoreInfo storeInfo = new PcmStoreInfo();
+        storeInfo.setStoreCode(storeCode);
+        List<PcmStoreInfo> storeList = selectListByParam(storeInfo);
+        if (storeList != null && storeList.size() > 0) {
+            String field5 = storeList.get(0).getField5();
+            if (field5 == null || "1".equals(field5)) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return true;
+        }
+    }
 
-	/**
-	 * 门店信息分页查询
-	 *
-	 * @param queryDto
-	 * @return
-	 */
-	@Override
-	public Page<PcmStoreInfoResultDto> findPageStoreInfo(PcmStoreInfoQueryDto queryDto) {
-		logger.debug("start findPageStoreInfo(),param:" + queryDto.toString());
-		Page<PcmStoreInfoResultDto> page = new Page<PcmStoreInfoResultDto>();
+    /**
+     * 门店信息分页查询
+     *
+     * @param queryDto
+     * @return
+     */
+    @Override
+    public Page<PcmStoreInfoResultDto> findPageStoreInfo(PcmStoreInfoQueryDto queryDto) {
+        logger.debug("start findPageStoreInfo(),param:" + queryDto.toString());
+        Page<PcmStoreInfoResultDto> page = new Page<PcmStoreInfoResultDto>();
 
-		Integer currentPage = queryDto.getCurrentPage();
-		Integer pageSize = queryDto.getPageSize();
-		if (currentPage != null) {
-			page.setCurrentPage(currentPage);
-		} else {
-			page.setCurrentPage(Constants.PUBLIC_1);
-		}
+        Integer currentPage = queryDto.getCurrentPage();
+        Integer pageSize = queryDto.getPageSize();
+        if (currentPage != null) {
+            page.setCurrentPage(currentPage);
+        } else {
+            page.setCurrentPage(Constants.PUBLIC_1);
+        }
 
-		if (pageSize != null) {
-			page.setPageSize(pageSize);
-		} else {
-			page.setPageSize(Constants.PUBLIC_10);
-		}
+        if (pageSize != null) {
+            page.setPageSize(pageSize);
+        } else {
+            page.setPageSize(Constants.PUBLIC_10);
+        }
 
-		// 查询总数量
-		Integer count = storeInfoMapper.getPageCountByPara(queryDto);
-		page.setCount(count);
+        // 查询总数量
+        Integer count = storeInfoMapper.getPageCountByPara(queryDto);
+        page.setCount(count);
 
-		// 分页查询
-		queryDto.setStart(page.getStart());
-		queryDto.setLimit(page.getLimit());
-		List<PcmStoreInfoResultDto> storeInfoResultDtoList = storeInfoMapper
-				.selectPageByPara(queryDto);
+        // 分页查询
+        queryDto.setStart(page.getStart());
+        queryDto.setLimit(page.getLimit());
+        List<PcmStoreInfoResultDto> storeInfoResultDtoList = storeInfoMapper
+                .selectPageByPara(queryDto);
 
-		page.setList(storeInfoResultDtoList);
+        page.setList(storeInfoResultDtoList);
 
-		logger.info("end findPageStoreInfo(),return:" + storeInfoResultDtoList.toString());
-		return page;
-	}
+        logger.info("end findPageStoreInfo(),return:" + storeInfoResultDtoList.toString());
+        return page;
+    }
 
-	/**
-	 * 查询门店信息List
-	 *
-	 * @param queryDto
-	 * @return
-	 */
-	@Override
-	public List<PcmStoreInfoResultDto> findListStoreInfo(PcmStoreInfoQueryDto queryDto) {
-		logger.debug("start findListStoreInfo(),param:" + queryDto.toString());
-		List<PcmStoreInfoResultDto> storeInfoResultDtoList = storeInfoMapper
-				.selectPageByPara(queryDto);
-		logger.info("end findListStoreInfo(),return:" + storeInfoResultDtoList.toString());
-		return storeInfoResultDtoList;
-	}
+    /**
+     * 查询门店信息List
+     *
+     * @param queryDto
+     * @return
+     */
+    @Override
+    public List<PcmStoreInfoResultDto> findListStoreInfo(PcmStoreInfoQueryDto queryDto) {
+        logger.debug("start findListStoreInfo(),param:" + queryDto.toString());
+        List<PcmStoreInfoResultDto> storeInfoResultDtoList = storeInfoMapper
+                .selectPageByPara(queryDto);
+        logger.info("end findListStoreInfo(),return:" + storeInfoResultDtoList.toString());
+        return storeInfoResultDtoList;
+    }
 
-	/**
-	 * 供应商平台根据门店编码查询门店信息
-	 *
-	 * @param queryDto
-	 * @return
-	 */
-	@Override
-	public List<PcmStoreInfoToSupplierDto> findStoreInfoByParaToSupplier(
-			PcmStoreInfoQueryDto queryDto) {
-		logger.info("start findStoreInfoByParaToSupplier(),param:" + queryDto.toString());
-		List<PcmStoreInfoToSupplierDto> resultList = new ArrayList<PcmStoreInfoToSupplierDto>();
+    /**
+     * 供应商平台根据门店编码查询门店信息
+     *
+     * @param queryDto
+     * @return
+     */
+    @Override
+    public List<PcmStoreInfoToSupplierDto> findStoreInfoByParaToSupplier(
+            PcmStoreInfoQueryDto queryDto) {
+        logger.info("start findStoreInfoByParaToSupplier(),param:" + queryDto.toString());
+        List<PcmStoreInfoToSupplierDto> resultList = new ArrayList<PcmStoreInfoToSupplierDto>();
 
-		List<PcmStoreInfoResultDto> resultDtoList = storeInfoMapper.findListByPara(queryDto);
-		if (resultDtoList != null && resultDtoList.size() > 0) {
-			for (PcmStoreInfoResultDto resultDto : resultDtoList) {
-				PcmStoreInfoToSupplierDto toSupplierDto = new PcmStoreInfoToSupplierDto();
-				toSupplierDto.setAddress(resultDto.getRegisteredAddress());
-				toSupplierDto.setBank(resultDto.getBank());
-				toSupplierDto.setBankAccount(resultDto.getBankAccount());
-				toSupplierDto.setFax(resultDto.getFaxNumber());
-				toSupplierDto.setLegal(resultDto.getLegalRepresentative());
-				toSupplierDto.setPostCode(resultDto.getPostCode());
-				toSupplierDto.setProxy(resultDto.getAgent());
-				toSupplierDto.setShopName(resultDto.getOrganizationName());
-				toSupplierDto.setTaxNo(resultDto.getTaxRegistrationNumber());
-				toSupplierDto.setTel(resultDto.getTelephoneNumber());
-				resultList.add(toSupplierDto);
-			}
-		}
-		logger.info("end findStoreInfoByParaToSupplier(),return:" + resultList.toString());
-		return resultList;
-	}
+        List<PcmStoreInfoResultDto> resultDtoList = storeInfoMapper.findListByPara(queryDto);
+        if (resultDtoList != null && resultDtoList.size() > 0) {
+            for (PcmStoreInfoResultDto resultDto : resultDtoList) {
+                PcmStoreInfoToSupplierDto toSupplierDto = new PcmStoreInfoToSupplierDto();
+                toSupplierDto.setAddress(resultDto.getRegisteredAddress());
+                toSupplierDto.setBank(resultDto.getBank());
+                toSupplierDto.setBankAccount(resultDto.getBankAccount());
+                toSupplierDto.setFax(resultDto.getFaxNumber());
+                toSupplierDto.setLegal(resultDto.getLegalRepresentative());
+                toSupplierDto.setPostCode(resultDto.getPostCode());
+                toSupplierDto.setProxy(resultDto.getAgent());
+                toSupplierDto.setShopName(resultDto.getOrganizationName());
+                toSupplierDto.setTaxNo(resultDto.getTaxRegistrationNumber());
+                toSupplierDto.setTel(resultDto.getTelephoneNumber());
+                resultList.add(toSupplierDto);
+            }
+        }
+        logger.info("end findStoreInfoByParaToSupplier(),return:" + resultList.toString());
+        return resultList;
+    }
 
 }
